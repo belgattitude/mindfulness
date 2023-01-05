f9b988cdde7e9c7d64bc5fda6154fa5c1302fdac
Sébastien Vanvelthem <belgattitude@gmail.com>
2023-01-05 17:09:08 +0100

    feat: add csp

--- web/next.config.mjs
+++ web/next.config.mjs
@@ -1,14 +1,57 @@
 // @ts-check
 
 import withBundleAnalyzer from '@next/bundle-analyzer';
+import { createSecureHeaders } from 'next-secure-headers';
 
 const isProd = process.env.NODE_ENV === 'production';
+const enableCSP = isProd;
+
+// @link https://github.com/jagaapple/next-secure-headers
+const secureHeaders = createSecureHeaders({
+  contentSecurityPolicy: {
+    directives: enableCSP
+      ? {
+        defaultSrc: "'self'",
+        styleSrc: [
+          "'self'",
+          "'unsafe-inline'",
+        ],
+        scriptSrc: [
+          "'self'",
+          "'unsafe-eval'",
+          "'unsafe-inline'",
+        ],
+        frameSrc: [
+          "'self'",
+        ],
+        connectSrc: [
+          "'self'",
+          'https://vitals.vercel-insights.com',
+          'http://mindfulness.failwell.be',
+          'https://mindfulness.failwell.be',
+        ],
+        imgSrc: ["'self'", 'https:', 'http:', 'data:'],
+        workerSrc: ['blob:'],
+      }
+      : {},
+  },
+  ...(enableCSP
+    ? {
+      forceHTTPSRedirect: [
+        true,
+        { maxAge: 60 * 60 * 24 * 4, includeSubDomains: true },
+      ],
+    }
+    : {}),
+  referrerPolicy: 'same-origin',
+});
+
 
 /** @type {import('next').NextConfig} */
 let nextConfig = {
   reactStrictMode: true,
   /**
-  webpack(config, context) {
+   webpack(config, context) {
     if (context.buildId !== "development") {
       // In monorepo example when the locales live in a separate package
       // there's still issues regarding fast-refresh... To investigate
@@ -30,6 +73,21 @@ let nextConfig = {
       },
     ],
   },
+
+  async headers() {
+    return [
+      {
+        // All page routes, not the api ones
+        source: '/:path((?!api).*)*',
+        headers: [
+          ...secureHeaders,
+          { key: 'Cross-Origin-Opener-Policy', value: 'same-origin' },
+          { key: 'Cross-Origin-Embedder-Policy', value: 'same-origin' },
+        ],
+      },
+    ];
+  },
+
 
   experimental: {
     // https://nextjs.org/docs/messages/google-fonts-missing-subsets
